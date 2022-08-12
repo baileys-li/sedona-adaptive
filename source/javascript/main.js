860655487b1105e7165c3c028572bd3e6f6c646e
Arthur Baileys Li <arthurlitovko@gmail.com>
2022-08-12 23:07:53 +0300

    Add script to open nav

---
+++ source/javascript/main.js
@@ -0,0 +1,18 @@
+const toggler = document.querySelector(".nav-toggler");
+const navigation = toggler.ariaControlsElements[0];
+
+/* Init nav toggler */
+toggler.hidden = false;
+navigation.classList.add("header__nav--enabled");
+toggler.addEventListener("click", changeNavState);
+
+function changeNavState() {
+	const shouldBeOpened = toggler.ariaExpanded === "false";
+
+	toggler.ariaExpanded = String(shouldBeOpened);
+	const label = `${shouldBeOpened ? "Закрыть" : "Открыть"} навигацию`;
+	toggler.ariaLabel = label;
+	toggler.title = label;
+
+	navigation.classList.toggle("header__nav--opened", shouldBeOpened);
+}
